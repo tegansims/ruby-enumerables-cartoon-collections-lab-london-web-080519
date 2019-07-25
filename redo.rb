fa12440471567bd16dea9416f0fbe534fd7f8d1f
Tegan Sims <tegansims@hotmail.com>
2019-07-25 12:39:53 +0000

    Automatically backed up by Learn

--- redo.rb
+++ redo.rb
@@ -1,23 +1,23 @@
 def roll_call_dwarves(array)
-  array.each_with_index { |item, x| 
-    puts "#{x+1} #{item}"
-  }
+ 
 end
 
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 roll_call_dwarves(dwarves)
 
+puts "## ------------------------- ##"
 
 def summon_captain_planet(array)
-  p array.map { |x| x.capitalize + "!"}
+  
 end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 summon_captain_planet(planeteer_calls)
 
+puts "## ------------------------- ##"
 
 def long_planeteer_calls(array)
-  p array.any? {|x| x.length > 4 }
+  
 end
 
 short_words = ["puff", "go", "two"]
@@ -27,14 +27,9 @@ assorted_words = ["two", "go", "industrious", "bop"]
 long_planeteer_calls(assorted_words)
 
 
+puts "## ------------------------- ##"
 def find_the_cheese(array)
-  if array.include?("cheddar")
-    return x
-  elsif array.include?("gouda")
-    return x 
-  elsif array.include?("thyme")
-    return x 
-  end
+
 end
 
 snacks = ["crackers", "gouda", "thyme"]
